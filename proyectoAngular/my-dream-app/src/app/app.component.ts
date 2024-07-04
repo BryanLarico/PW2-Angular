11b7930496a2148f71138c45b84a7d6f7b329014
BryanLarico <blaricor@unsa.edu.pe>
2024-07-04 17:35:09 -0500

    Pasando datos al Componente user: ahora el template user.component.html puede usar nameUser

--- proyectoAngular/my-dream-app/src/app/app.component.ts
+++ proyectoAngular/my-dream-app/src/app/app.component.ts
@@ -2,36 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserComponent } from './user/user.component' 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HelloWorldComponent],
+  imports: [
+		RouterOutlet,
+		CommonModule, 
+		HelloWorldComponent,
+		UserComponent,
+	],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-dream-app';
-	name : string;
-	email;
-	webpage : string;
-	hobbies : string[];
-	showHobbies : boolean;
-	
-	constructor() {
-		console.log("Constructor working...");
-		this.name = "Bryan Larico R.";
-		this.email = "blaricor@unsa.edu.pe";
-		this.webpage = "http://www.unsa.edu.pe";
-		this.hobbies = ["Voley","Programacion","Netflix"];
-		this.showHobbies = false;
-	}
-	toggleHobbies() {
-		this.showHobbies = !this.showHobbies;
-	}
-	newHobby(hobby:any) {
-		this.hobbies.push(hobby.value);
-		hobby.value = "";
-		return false;
-	}
+  users = ['jhon','jhonny','joe','cameron'];
+	activated = false;
 }
