b64f306fa7f4308bd3ac5eb20275454e476549a5
BryanLarico <blaricor@unsa.edu.pe>
2024-07-08 22:50:48 -0500

    Insertando un Servicio para llamar a Json: el JSON aparece en la consola

--- proyectoAngular/my-dream-app/src/app/app.component.ts
+++ proyectoAngular/my-dream-app/src/app/app.component.ts
@@ -1,24 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UserComponent } from './user/user.component';
 import { FormsModule } from '@angular/forms';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-		RouterOutlet,
-		CommonModule, 
-		HelloWorldComponent,
-		UserComponent,
-		FormsModule,
-	],
+    RouterOutlet,
+    CommonModule, 
+    HelloWorldComponent,
+    UserComponent,
+    FormsModule,
+    HttpClientModule,  // Asegúrate de importar HttpClientModule aquí
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DataService]  // Asegúrate de que DataService está en providers
 })
 export class AppComponent {
-  name : string = "Bryan Larico R.";
-	age : number = 40;
+  name: string = "Bryan Larico R.";
+  age: number = 40;
+  
+  constructor(private dataService: DataService) {
+    this.dataService.getData().subscribe(data => {
+      console.log(data);
+    });
+  }
 }
--- proyectoAngular/my-dream-app/src/app/app.module.ts
+++ proyectoAngular/my-dream-app/src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [],
   imports: [
     BrowserModule,
-		AppRoutingModule,
-		FormsModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
